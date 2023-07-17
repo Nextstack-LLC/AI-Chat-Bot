70345c49abe86d6ce769003152a6deb53f4ced07
1993grigoryan <garikgrigoryan@nextstack.org>
2023-07-17 16:20:11 +0400

    AI chat bot

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ChatBot from "components/chat";
+import "./styles/main.scss";
+
+function App() {
+  return <ChatBot />;
+}
+
+export default App;
---
+++ src/faq.js
@@ -0,0 +1,18 @@
+export const faq = [
+  {
+    id: 1,
+    label: "What services do you provide?",
+  },
+  {
+    id: 2,
+    label: "NextStack’s experience and portfolio",
+  },
+  {
+    id: 3,
+    label: "What technologies do you use?",
+  },
+  {
+    id: 4,
+    label: "Talk to sales",
+  },
+];
