a343aec2aceae91ff7b9b4e17012cb47a8486dc5
1993grigoryan <garikgrigoryan@nextstack.org>
2023-08-03 19:28:05 +0400

    css fix

--- dist/chatBot.js
+++ dist/chatBot.js
@@ -7,16 +7,11 @@ exports["default"] = void 0;
 var _react = _interopRequireDefault(require("react"));
 var _chat = _interopRequireDefault(require("./chat"));
 var _ErrorBoudary = _interopRequireDefault(require("./ErrorBoudary"));
-var _index = _interopRequireDefault(require("!!raw-loader!ns-bot/dist/styles/index.css"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 function ChatBot(props) {
   return /*#__PURE__*/_react["default"].createElement(_ErrorBoudary["default"], {
     chatchError: props.chatchError
-  }, /*#__PURE__*/_react["default"].createElement("style", {
-    dangerouslySetInnerHTML: {
-      __html: _index["default"]
-    }
-  }), /*#__PURE__*/_react["default"].createElement(_chat["default"], {
+  }, /*#__PURE__*/_react["default"].createElement(_chat["default"], {
     companyId: props.companyId,
     analitics: props.analitics,
     trackImpression: props.trackImpression
--- src/chatBot.js
+++ src/chatBot.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Chat from "./chat";
 import ErrorBoundary from "./ErrorBoudary";
-import chatBotStyles from '!!raw-loader!ns-bot/dist/styles/index.css';
 
 function ChatBot(props) {
   return ( 
     <ErrorBoundary chatchError={props.chatchError}>
-      <style dangerouslySetInnerHTML={{ __html: chatBotStyles }} />
       <Chat
         companyId={props.companyId}
         analitics={props.analitics}
