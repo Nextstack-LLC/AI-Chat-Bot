71827674d67fd9363acad300eab015a4269cd49b
1993grigoryan <garikgrigoryan@nextstack.org>
2023-08-03 17:31:05 +0400

    add scss

---
+++ dist/chatBot.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+var _chat = _interopRequireDefault(require("./chat"));
+var _ErrorBoudary = _interopRequireDefault(require("./ErrorBoudary"));
+require("./styles/main.scss");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function ChatBot(props) {
+  return /*#__PURE__*/_react["default"].createElement(_ErrorBoudary["default"], {
+    chatchError: props.chatchError
+  }, /*#__PURE__*/_react["default"].createElement(_chat["default"], {
+    companyId: props.companyId,
+    analitics: props.analitics,
+    trackImpression: props.trackImpression
+  }));
+}
+var _default = ChatBot;
+exports["default"] = _default;
