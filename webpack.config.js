d6af173625ca5b12377144f1349d0524e2e140dd
1993grigoryan <garikgrigoryan@nextstack.org>
2023-08-04 18:42:31 +0400

    webpack config fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,24 @@
 const path = require('path');
-const webpack = require('webpack');
-const nodeExternals = require('webpack-node-externals');
 
-
-
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-  const config = {
-    target: isProduction ? 'browserslist' : 'web',
-    mode: "production",
-    entry: "./src/index.js",
-    output: {
-      path:  path.resolve(__dirname, "build"),
-      filename: "index.js",
-      publicPath: "/",
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"]
-        }
-      ]
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.IS_BROWSER': JSON.stringify(!isProduction), // Indicate that it's a browser environment
-      }),
-    ],
-    externals: {
-      react: "react"
-    },
-  };
-
-  if (!isProduction) {
-    // Add server-side rendering configuration
-    config.target = 'node';
-    config.externals = [nodeExternals()];
-    config.output.libraryTarget = 'commonjs2';
-  }
-
-  return config;
-}
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path:  path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  externals: {
+    react: "react"
+  },
+};
