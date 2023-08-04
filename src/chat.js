aab5565caa9203b219aabe83f0a46b1696c2b4a8
1993grigoryan <garikgrigoryan@nextstack.org>
2023-08-04 17:12:22 +0400

    add condition

--- src/chat.js
+++ src/chat.js
@@ -77,103 +77,99 @@ export default function MyChatBot(props) {
 
   return (
     <>
-      {typeof document !== 'undefined' && (
-        <>
-          {isOpen ? (
-            <div className="chat-container">
-              <div className="chat-header">
-                <div>
-                  <img className="avatar" src="./icons/avatar.webp" alt="Avatar" />
-                  <p className="company-name">{CONSTANTS.NEXTSTACK_LLC}</p>
-                </div>
-                <div
-                  onClick={() => {
-                    setIsOpen(false)
-                    handleTrackImpression(false)
-                  }}
-                  className="close-container"
-                >
-                  <img
-                    className="close"
-                    src="./icons/close.webp" alt="Close"
-                  />
-                </div>
-              </div>
-              {(!isSelected && chatHistory.length < 2) && (
-                <div className="faq-container">
-                  {faq.map((item) => (
-                    <div 
-                      onClick={() => selectQuestion(item.label)}
-                      className="user-message" key={item.id}
-                    >
-                      <p className="faq-text">{item.label}</p>
-                    </div>
-                  ))}
-                </div>
-              )}
-              <div className="chat-body">
-                <ul>
-                    {chatHistory.map((item, index) => (
-                      <li key={index}>
-                        {item.message && (
-                          <div className="user-message">
-                            <p>{item.message}</p>
-                          </div>
-                        )}
-                        {item.response && (
-                          <div className="bot-answer">
-                            <p>{item.response}</p>
-                            {item.icon && <img src={item.icon} alt="Hand" />}
-                          </div>
-                        )}
-                      </li>
-                    ))}
-                </ul>
-                {loading && (
-                  <div className="loading">
-                    <div className="dot-flashing" />
-                  </div>
-                )}
-              </div>
-              <div className="form">
-                <span className="line" />
-                <div className="input-container">
-                  <textarea
-                    disabled={loading}
-                    rows={6} 
-                    placeholder="Enter your message"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    onKeyDown={(e) => {
-                      if (e.code === "Enter") {
-                        e.preventDefault();
-                        handleSendMessage();
-                      }
-                    } }
-                  />
-                  <button 
-                    disabled={loading}
-                    onClick={handleSendMessage}
-                    className={`send-button ${message && 'send-button-red'}`}
-                  >
-                    <img src={message ? './icons/send_red.webp' : './icons/send.webp'} alt="Send" />
-                  </button>
-                </div>
-              </div>
+      {isOpen ? (
+        <div className="chat-container">
+          <div className="chat-header">
+            <div>
+              <img className="avatar" src="./icons/avatar.webp" alt="Avatar" />
+              <p className="company-name">{CONSTANTS.NEXTSTACK_LLC}</p>
             </div>
-          ) : (
-            <div 
-              className="chat-bot-icon" 
+            <div
               onClick={() => {
-                setIsOpen(true)
-                handleTrackImpression(true)
+                setIsOpen(false)
+                handleTrackImpression(false)
               }}
+              className="close-container"
             >
-              <img src="./icons/lets_talk.webp" alt="Let's Talk" />
-              <p className="lets-talk-txt">{CONSTANTS.LETS_TALK}</p>
+              <img
+                className="close"
+                src="./icons/close.webp" alt="Close"
+              />
+            </div>
+          </div>
+          {(!isSelected && chatHistory.length < 2) && (
+            <div className="faq-container">
+              {faq.map((item) => (
+                <div 
+                  onClick={() => selectQuestion(item.label)}
+                  className="user-message" key={item.id}
+                >
+                  <p className="faq-text">{item.label}</p>
+                </div>
+              ))}
             </div>
           )}
-        </>
+          <div className="chat-body">
+            <ul>
+                {chatHistory.map((item, index) => (
+                  <li key={index}>
+                    {item.message && (
+                      <div className="user-message">
+                        <p>{item.message}</p>
+                      </div>
+                    )}
+                    {item.response && (
+                      <div className="bot-answer">
+                        <p>{item.response}</p>
+                        {item.icon && <img src={item.icon} alt="Hand" />}
+                      </div>
+                    )}
+                  </li>
+                ))}
+            </ul>
+            {loading && (
+              <div className="loading">
+                <div className="dot-flashing" />
+              </div>
+            )}
+          </div>
+          <div className="form">
+            <span className="line" />
+            <div className="input-container">
+              <textarea
+                disabled={loading}
+                rows={6} 
+                placeholder="Enter your message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.code === "Enter") {
+                    e.preventDefault();
+                    handleSendMessage();
+                  }
+                } }
+              />
+              <button 
+                disabled={loading}
+                onClick={handleSendMessage}
+                className={`send-button ${message && 'send-button-red'}`}
+              >
+                <img src={message ? './icons/send_red.webp' : './icons/send.webp'} alt="Send" />
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div 
+          className="chat-bot-icon" 
+          onClick={() => {
+            setIsOpen(true)
+            handleTrackImpression(true)
+          }}
+        >
+          <img src="./icons/lets_talk.webp" alt="Let's Talk" />
+          <p className="lets-talk-txt">{CONSTANTS.LETS_TALK}</p>
+        </div>
       )}
     </>
   );
--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import Chat from "./chat";
 import ErrorBoundary from "./ErrorBoudary";
 
 function ChatBot(props) {
-  return ( 
-    <ErrorBoundary chatchError={props.chatchError}>
-      <Chat
-        companyId={props.companyId}
-        analitics={props.analitics}
-        trackImpression={props.trackImpression}
-      />
-    </ErrorBoundary>
-  )
+  if (typeof document !== 'undefined') {
+    return ( 
+      <ErrorBoundary chatchError={props.chatchError}>
+        <Chat
+          companyId={props.companyId}
+          analitics={props.analitics}
+          trackImpression={props.trackImpression}
+        />
+      </ErrorBoundary>
+    )
+  }
 }
 
 export default ChatBot;
